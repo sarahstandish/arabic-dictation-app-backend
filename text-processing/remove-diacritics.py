60f9af0324a7fb735529124ecad4cc9fd155b941
sarahstandish <z.sisyphusrocks@gmail.com>
2022-01-21 11:02:37 -0800

    add additional checks for words that are too short or end in alif maqsuura + tanween fatha

--- text-processing/remove-diacritics.py
+++ text-processing/remove-diacritics.py
@@ -1,34 +1,78 @@
 import csv
-
-"""
-Diacritics:
-U+0650 kesra
-U+064F damma
-U+064E kesra
-U+0651 shadda
-U+0652 sukuun
-U+0670 superscript alif
-"""
-
-diacritics = [f"\u0650", f"\u064F", f"\u064E", f"\u0651", f"\u0652", f"\u0670"]
+from distutils.command.check import check
+from pydoc import source_synopsis
 
 source_file = 'text-processing/byu-corpus.csv'
-destination_content = []
 destination_file = 'text-processing/voweled-and-unvoweled-words.csv'
 
-with open(source_file, newline='', encoding='utf-8') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        if row["Exclude"].lower() != 'yes':
-            voweled_word = row["Word"].strip()
-            unvoweled_word = ""
-            for char in voweled_word:
-                if char not in diacritics:
-                    unvoweled_word += char
-            destination_content.append({"voweled_word": voweled_word, "unvoweled_word": unvoweled_word})
-            
-with open(destination_file, 'w', newline='', encoding='utf-8') as csvfile:
-    fieldnames=['voweled_word', 'unvoweled_word']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
-    writer.writerows(destination_content)
+def remove_diacritics(source_file):
+
+    """
+    Diacritics:
+    U+0650 kesra
+    U+064F damma
+    U+064E kesra
+    U+0651 shadda
+    U+0652 sukuun
+    U+0670 superscript alif
+    U+064B tanween fatha
+    """
+
+    diacritics = ["\u0650", "\u064F", "\u064E", "\u0651", "\u0652", "\u0670", "\u064B"]
+    destination_content = []
+
+    with open(source_file, newline='', encoding='UTF-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if row["Exclude"].lower() != 'yes':
+                voweled_word = row["Word"].strip()
+                unvoweled_word = ""
+                for char in voweled_word:
+                    if char not in diacritics:
+                        unvoweled_word += char
+                voweled_word = remove_final_tanween_fatha(voweled_word)
+                # wow! apparently Excel can't interpret unicode utf-8 without a byte-order mark at the beginning
+                # so it's necessary to add U+FEFF at the beginning of the file
+                # otherwise it will be interpreted as ascii
+                destination_content.append({"\ufeffvoweled_word": voweled_word, "unvoweled_word": unvoweled_word})
+
+    return destination_content
+
+def check_for_latin_chars(row, voweled_word, char):
+    """
+    check for any remaining latin characters
+    """
+    if ord(char) < 127:
+        print("Latin letters found in", voweled_word)
+        print(row["\ufeffID"])
+    input()
+
+def check_for_short_words(row, word):
+    """
+    Check for presence of single-character words
+    """
+    if len(word) <= 2:
+
+        print(f"Short word {word} found in row", row["\ufeffID"])
+        input()
+
+def remove_final_tanween_fatha(word):
+    """
+    remove final tanween fatha from words ending in alif maqsuura
+    """
+    alif_maqsuura_tanween_fatha = "\u0649\u064B"
+    if word[-2:] == alif_maqsuura_tanween_fatha:
+        word = word[:-1]
+    return word
+
+def write_destination_content_to_file(destination_content, destination_file):
+
+    with open(destination_file, 'w', newline='') as csvfile:
+        fieldnames=['\ufeffvoweled_word', 'unvoweled_word']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(destination_content)
+
+destination_content = remove_diacritics(source_file)
+
+# write_destination_content_to_file(destination_content, destination_file)
