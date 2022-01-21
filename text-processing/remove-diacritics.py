57745552f71fd83c20362e81b2f5100d5fc278ba
sarahstandish <z.sisyphusrocks@gmail.com>
2022-01-21 11:36:20 -0800

    extract plurals from notes field

--- text-processing/remove-diacritics.py
+++ text-processing/remove-diacritics.py
@@ -73,6 +73,39 @@ def write_destination_content_to_file(destination_content, destination_file):
         writer.writeheader()
         writer.writerows(destination_content)
 
-destination_content = remove_diacritics(source_file)
+def extract_plurals(source_file):
+    """
+    Extract plurals from the corpus.
+    Plurals are found in the Notes column
+    If there is a plural, the Notes text would start with "pl. "
+    And the following character would be non-ascii
+    """
+    destination_file = "text-processing/plurals.csv"
+    destination_content = []
+
+    with open(source_file, newline='', encoding='UTF-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if row["Exclude"].lower() != 'yes':
+                if row["Notes"][:3] == "pl." and ord(row["Notes"][4]) > 127:
+                    split = row["Notes"].split()
+                    plural = split[1].strip()
+                    # remove commas from the end of words
+                    if plural[-1] == ",":
+                        plural = plural[:-1]
+                    # exclude words ending in tanween kesra
+                    tanween_kesra = "\u064D"
+                    if plural[-1] != tanween_kesra:
+                        destination_content.append({"\ufeffplural": plural})
+
+        with open(destination_file, 'w', newline='') as csvfile:
+            fieldnames=['\ufeffplural']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(destination_content)
+
+extract_plurals(source_file)
+
+# destination_content = remove_diacritics(source_file)
 
 # write_destination_content_to_file(destination_content, destination_file)
