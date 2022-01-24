e69ed8d9e9c9c3c5d4e22abf93f80d2a518327d9
sarahstandish <z.sisyphusrocks@gmail.com>
2022-01-24 11:54:32 -0800

    add basic structure

---
+++ app/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+from dotenv import load_dotenv
+
+db = SQLAlchemy()
+migrate = Migrate()
+load_dotenv()
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    if test_config is None:
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
+
+    from app.models.word import Word
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # import and register Blueprints here
+    # from .routes import tasks_bp, goals_bp, slack_bp
+    # app.register_blueprint(tasks_bp)
+
+    return app
---
+++ app/routes.py
@@ -0,0 +1,4 @@
+from flask import Blueprint, json, jsonify, request
+from .models.word import Word
+from app import db
+
