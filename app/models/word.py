c9d043b690cb94a0b26f7fd16bfb8154c387ab2c
sarahstandish <z.sisyphusrocks@gmail.com>
2022-01-26 10:27:58 -0800

    add function to take in query string and return a string with letters to exclude

--- app/models/word.py
+++ app/models/word.py
@@ -67,5 +67,65 @@ class Word(db.Model):
         return len(l) <= cls.max_return_list_len
 
     @staticmethod
-    def get_letter_filter_array(letters):
-        return list(letters)
+    def get_letter_string(letters):
+        """
+        Letters is a string from the url query params.
+        Change these letters to a set.
+        Subtract that set from the set of all possible letters.
+        Transform the set of all remaining letters to a string in form "%(letter|letter|letter)%" etc. Return it.
+        """
+        all_letters = {
+            "\u0621", # independent hamza
+            "\u0622", # alif medda
+            "\u0623", # alif with hamza on top
+            "\u0625", # alif with hamza on bottom
+            "\u0624", # waw with hamza on top
+            "\u0626", # yaa with hamza on top
+            "\u0627", # alif
+            "\u0628", # baa
+            "\u0629", # taa marbuuta
+            "\u062A", # taa
+            "\u062B", # thaa
+            "\u062C", # jeem
+            "\u062D", # Haa
+            "\u062E", # khaa
+            "\u062F", # daal
+            "\u0630", # dhaal,
+            "\u0631", # raa,
+            "\u0632", # zay
+            "\u0633", # seen
+            "\u0634", # sheen
+            "\u0635", # saad
+            "\u0636", # daad
+            "\u0637", # taa
+            "\u0638", # dhaa
+            "\u0639", # ayn
+            "\u063A", # ghayn
+            "\u0641", # faa
+            "\u0642", # qaaf
+            "\u0643", # kaaf
+            "\u0644", # laam
+            "\u0645", # meem
+            "\u0646", # nuun
+            "\u0647", # haa
+            "\u0648", # waaw
+            "\u064A", # yaa
+            "\u0649", # alif maqsuura
+        }
+        query_param_letters = set(list(letters))
+
+        letters_to_filter_out = all_letters - query_param_letters
+
+        letters_to_filter_out = list(letters_to_filter_out)
+
+        similar_to_string = "%("
+
+        for char in letters_to_filter_out:
+            similar_to_string += char
+            similar_to_string += "|"
+
+        # remove the final pipe character
+        similar_to_string = similar_to_string[:-1]
+        similar_to_string += ")%"
+
+        return similar_to_string
