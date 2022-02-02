3d1d3e06dccf2fd82cc2942fbdc72f14109c4615
sarahstandish <z.sisyphusrocks@gmail.com>
2022-02-02 08:25:47 -0800

    remove file when done

--- text-processing/create_audio_file_upload_word.py
+++ text-processing/create_audio_file_upload_word.py
@@ -1,12 +1,13 @@
 from get_audio_file import *
 from upload_file import *
 from get_word_to_pronounce import *
+import os
 
-def create_audio_file_upload_word(word):
+def create_audio_file_upload_word(voweled_word):
 
-    word_to_pronounce = get_word_to_pronounce(word)
+    word_to_pronounce = get_word_to_pronounce(voweled_word)
 
-    file_name = hashlib.md5(bytes(word, 'utf-8')).hexdigest()
+    file_name = hashlib.md5(bytes(voweled_word, 'utf-8')).hexdigest()
 
     audio_file_loc = f"./text-processing/audio_files/{file_name}.mp3"
 
@@ -15,3 +16,5 @@ def create_audio_file_upload_word(word):
     bucket_name = "arabic-dictation-app"
 
     upload_blob(bucket_name, audio_file_loc, file_name)
+
+    os.remove(audio_file_loc)
